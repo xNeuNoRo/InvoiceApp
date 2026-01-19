9332c97c8dc245329a4ef3c96ef2775f85360880
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-01-18 22:56:41 -0400

    Add IProductRepository interface with CRUD methods

--- contracts/IProductRepository.cs
+++ contracts/IProductRepository.cs
@@ -1,1 +1,13 @@
+using InvoiceApp.Domain;
+
 namespace InvoiceApp.Contracts;
+
+public interface IProductRepository
+{
+    void Create(Product product);
+    void UpdateById(int productId, Product updatedProduct);
+    void DeleteById(int productId);
+    Product? FindById(int productId);
+    List<Product> FindAll();
+    Product FindByName(string productName);
+}
