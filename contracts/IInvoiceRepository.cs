aae9e7b836a8ea548fc8281607a95a0a18425472
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-01-18 23:02:16 -0400

    Add IInvoiceRepository interface with CRUD methods

--- contracts/IInvoiceRepository.cs
+++ contracts/IInvoiceRepository.cs
@@ -1,1 +1,12 @@
+using InvoiceApp.Domain;
+
 namespace InvoiceApp.Contracts;
+
+public interface IInvoiceRepository
+{
+    void Create(Invoice invoice);
+    void UpdateById(int invoiceId, Invoice updatedInvoice);
+    void DeleteById(int invoiceId);
+    Invoice? FindById(int invoiceId);
+    List<Invoice> FindAll();
+}
